f943d492ad458230cf2b10ac46171266eb243c7a
Agostinho93 <jpfagostinho@gmail.com>
2025-12-04 12:24:58 +0000

    Publica build web com frase dos álbuns corrigida

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -27,7 +27,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "47dca9fc299b65bd2bed4e1da0a1b6e4
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "962a1fffe19d458bfed2dec16e596a10",
+"flutter_bootstrap.js": "747ce4608ec59a3c0e1d7d137b0b6c1f",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
